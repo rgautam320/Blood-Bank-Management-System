07a3107cb1ba536fcd5ecd8f89f23605eb66daca
Rajan Gautam <gautamrajan073@gmail.com>
2022-04-25 23:56:34 +0530

    Database Added

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -63,12 +63,24 @@ WSGI_APPLICATION = 'bloodbankmanagement.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'cloudcomputinglab',  
+        'USER': 'rgautam@rgautam-mysql-server',  
+        'PASSWORD': '@$dfghjkl123A',  
+        'HOST': 'rgautam-mysql-server.mysql.database.azure.com',  
+        'PORT': '3306'  
+    }  
+}  
 
 # Password validation
 
